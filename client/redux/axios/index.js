09c1691a33103abbc76326936396888e5717cdb3
Akolade <adesanmiakoladedotun@gmail.com>
2019-05-11 07:50:53 +0100

    feature(Endpoint, React and Redux): Create endpoints, ui pages and integrate Redux

--- client/redux/axios/index.js
+++ client/redux/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const { NODE_ENV } = process.env;
-const baseURL = NODE_ENV === 'production' ? 'https://bucketlist-api.herokuapp.com/api/v1/' : 'http://localhost:5900/api/v1/'; // eslint-disable-line
+const baseURL = NODE_ENV === 'production' ? 'https://bucketlist-ap.herokuapp.com/api/v1/' : 'http://localhost:5900/api/v1/'; // eslint-disable-line
 
 const instance = axios.create({
   baseURL,
